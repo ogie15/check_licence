f75c26e4aa59a457b5e3e2b1c24a95b05fb0d143
Okougbo Ibhadogiemu <iokougbo@gmail.com>
2020-04-12 16:51:46 +0100

    Add files via upload
    Version 2

---
+++ Serv2.ps1
@@ -0,0 +1,101 @@
+#region working
+
+$Header1 = "UserPrincipalName," + "ServiceName," + "ServiceType," + "TargetClass," + "ProvisioningStatus," + "LicenseName,"
+
+$Header1 | Out-File -FilePath .\LicenseLog.txt
+
+$AllUsers = Get-MsolUser -All
+
+$UsersUPN = $AllUsers | Select-Object UserPrincipalName
+
+foreach ($Upn in $UsersUPN) {
+    $SingleUser = $Upn.UserPrincipalName.ToString()
+    
+    [string]$Store
+
+    $Total = @()
+
+    $Skus = (((Get-MsolUser -UserPrincipalName $SingleUser).Licenses).AccountSku).SkuPartNumber
+    
+    Write-Output $SingleUser
+
+    ForEach ($Sku in $Skus) {
+        # Write-Output $Sku
+
+        # Write-Output "oj"
+        
+        $ServiceNames = ((((Get-MsolUser -UserPrincipalName $SingleUser).Licenses).ServiceStatus).ServicePlan).ServiceName 
+
+        $ServiceTypes = ((((Get-MsolUser -UserPrincipalName $SingleUser).Licenses).ServiceStatus).ServicePlan).ServiceType
+
+        $TargetClass = ((((Get-MsolUser -UserPrincipalName $SingleUser).Licenses).ServiceStatus).ServicePlan).TargetClass
+
+        $ProvisioningStatus = (((Get-MsolUser -UserPrincipalName $SingleUser).Licenses).ServiceStatus).ProvisioningStatus
+
+        $FinalServiceName = @()
+
+        $FinalServiceType = @()
+
+        $FinalTargetClass = @()
+
+        $FinalProvisioningStatus = @()
+
+        Foreach ($ServiceName in $ServiceNames) {
+
+            $StoreServiceName = $ServiceName.ToString() + ","
+
+            $FinalServiceName += $StoreServiceName
+        }
+        
+        foreach ($ServiceType in $ServiceTypes) {
+
+            $StoreServiceType = $ServiceType.ToString() + ","
+
+            $FinalServiceType += $StoreServiceType
+
+        }
+
+        foreach ($TargetClasses in $TargetClass) {
+
+            $StoreTargetClasses = $TargetClasses.ToString() + ","
+
+            $FinalTargetClass += $StoreTargetClasses
+
+        }
+
+        foreach ($ProvisioningStat in $ProvisioningStatus) {
+
+            $StoreProvisioningStat = $ProvisioningStat.ToString() + ","
+
+            $FinalProvisioningStatus += $StoreProvisioningStat
+
+        }
+
+        $Count = $FinalServiceName.Count
+
+        $NewCount = $Count-1
+        # Write-Output $Count
+
+        $Looper = @(0..$NewCount)
+
+        foreach ($Loop in $Looper) {
+
+            if ($Loop -eq 0) {
+
+                $Store = $SingleUser + "," + $Sku + "," + $FinalServiceName[$Loop] + $FinalServiceType[$Loop] + $FinalTargetClass[$Loop] + $FinalProvisioningStatus[$Loop]
+
+                $Total += $Store
+            }
+            else {
+               $Store = " "+ "," + " " + "," + $FinalServiceName[$Loop] + $FinalServiceType[$Loop] + $FinalTargetClass[$Loop] + $FinalProvisioningStatus[$Loop]
+
+                $Total += $Store
+            }
+        }      
+        Write-Output $Total  
+        $Total | Out-File -FilePath .\LicenseLog.txt -Append
+    }
+}
+Import-Csv .\LicenseLog.txt -delimiter ','| Export-Csv -Path .\LicenseLog.csv -NoTypeInformation
+Remove-Item -Path .\LicenseLog.txt -Force
+#endregion working
